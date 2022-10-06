599e11ed6207fb40646e2656ca99307df6fb5df3
Pingvu <nhlpool27@gmail.com>
2022-10-06 13:34:43 +0200

    Add project files.

---
+++ upggift 3.14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace uppgift_3_14
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet?");
+            Console.WriteLine("svara j eller n");
+            string svar = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            string x = Console.ReadLine();
+            int a = int.Parse(x);
+            if (svar == "j" && a < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig.");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvär efter annan personal just nu.");
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
